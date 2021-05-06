6f8c8d7ca7fe53957f5ecf0419acb7ff7ded61e6
Abbos <yulchiyevabbos1@gamil.com>
2021-05-06 10:02:34 +0500

    *1

---
+++ src/test/java/pdp/uz/clickup/ClickUpApplicationTests.java
@@ -0,0 +1,13 @@
+package pdp.uz.clickup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
